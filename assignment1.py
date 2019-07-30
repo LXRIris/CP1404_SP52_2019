0db7c6b35fb4f29872d8ac1005795de53f242885
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2019-07-31 01:56:55 +0800

    finish everything except rewriting to file

--- assignment1.py
+++ assignment1.py
@@ -5,7 +5,8 @@ Date started: Does this even really matter
 GitHub URL: https://github.com/nelsonnyan2001/SP52_Prog2_Attempt
 """
 
-places_file = open('places.csv', 'r')
+csv_file = 'places.csv'
+places_file = open(csv_file, 'r')
 places_list_n = places_file.readlines()
 places_list = [i.replace("\n", '') for i in places_list_n]
 places_file.close()
@@ -28,14 +29,16 @@ def main_menu():
     print("M - Mark new place as visited")
     print("Q - Quit")
     print("")
-    menu_choice = input(">>>")
+    menu_choice_l = input(">>>")
+    print("")
     choices = ["l", "a", "m", "q"]
-    menu_choice_l = menu_choice.lower()
-    while menu_choice_l not in choices:
+    menu_choice = menu_choice_l.lower()
+    while menu_choice not in choices:
         print("Invalid menu choice.")
         print("")
-        menu_choice = input(">>>")
-        menu_choice_l = menu_choice.lower()
+        menu_choice_l = input(">>>")
+        print("")
+        menu_choice = menu_choice_l.lower()
     while menu_choice != "q":
         if menu_choice == "l":
             print_places()
@@ -43,21 +46,31 @@ def main_menu():
         if menu_choice == "a":
             add_menu()
             main_menu()
+        if menu_choice == "m":
+            mark_menu()
+            main_menu()
+
+
+    exit("")
 
 
 def print_places():
-    print("")
     to_visit_count = 0
     for i in range(len(individual_places_list)):
         if individual_places_list[i][3] == "n":
             to_visit_count += 1
-            print("*{}. {:<8} in {:<11} priority {}.".format(i, individual_places_list[i][0], individual_places_list[i][1],
-                                                       individual_places_list[i][2]))
+            print("*{}. {:<8} in {:<11} priority {}.".format(i+1, individual_places_list[i][0],
+                                                             individual_places_list[i][1],
+                                                             individual_places_list[i][2]))
         else:
-            print(" {}. {:<8} in {:<11} priority {}.".format(i, individual_places_list[i][0], individual_places_list[i][1],
-                                                         individual_places_list[i][2]))
+            print(" {}. {:<8} in {:<11} priority {}.".format(i+1, individual_places_list[i][0],
+                                                             individual_places_list[i][1],
+                                                             individual_places_list[i][2]))
     print("")
-    print(" {} places. You still want to visit {} places.".format(len(individual_places_list), to_visit_count))
+    if to_visit_count > 0:
+        print(" {} places. You still want to visit {} places.".format(len(individual_places_list), to_visit_count))
+    else:
+        print(" {} places. No places left to visit. WHy not add some more?".format(len(individual_places_list)))
     print("")
 
 
@@ -72,10 +85,12 @@ def add_menu():
         new_country = input("Country: ")
     new_priority = ask_for_number("Please input a priority: ")
     print("{} in {} (priority {}) added to Travel Tracker".format(new_name, new_country, new_priority))
+    print("")
     new_place = []
     new_place.append(new_name)
     new_place.append(new_country)
     new_place.append(new_priority)
+    new_place.append("n")
     individual_places_list.append(new_place)
 
 
@@ -84,12 +99,40 @@ def ask_for_number(message):
     try:
         number = int(input(message))
         while number <= 0:
-            print("Priority must be more than 0.")
+            print("Number must be more than 0.")
             number = int(input(message))
     except ValueError:
         print("Invalid input, please enter a valid number.")
         ask_for_number(message)
     return number
+
+
+def mark_menu():
+    print_places()
+    print("Enter the number of the place to mark as visited.")
+    ask_number = ask_for_number(">>>")
+    mark_number = ask_number-1
+    non_visited_count = 0
+    for i in range(len(individual_places_list)):
+        if individual_places_list[i][3] == "n":
+            non_visited_count += 1
+    if non_visited_count == 0:
+        print("No unvisited places.")
+
+    while mark_number > len(individual_places_list):
+        print("There aren't that many places.")
+        print("Enter the number of the place to mark as visited.")
+        ask_number = ask_for_number(">>>")
+        mark_number = ask_number - 1
+    while individual_places_list[mark_number][3] == "v":
+        print("You have already visited that place.")
+        print("Enter the number of the place to mark as visited.")
+        ask_number = ask_for_number(">>>")
+        mark_number = ask_number - 1
+    else:
+        individual_places_list[mark_number][3] = "v"
+        print("{} in {} visited.".format(individual_places_list[mark_number][0], individual_places_list[mark_number][1]))
+        print("")
 
 
 
