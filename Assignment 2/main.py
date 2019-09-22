4a5d2a68faac1276070835d5b7d9a9a75d47bfab
Zwe Nyan Toe <zwenyantoe@my.jcu.edu.au>
2019-09-22 19:39:00 +0800

    Finished the entire layout for the left column.

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -11,6 +11,8 @@ GitHub URL:
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from places_list import PlacesList
 
@@ -21,16 +23,16 @@ class TravelTracker(App):
         super().__init__()
         self.places_list = PlacesList()
         self.sort_label = Label(text="Sort by:")
-        self.filler1 = Label(text="Bruh")
+        self.spinner = Spinner(text="Place", values = ("Place", "Country", "Priority", "Visited"))
         self.add_label = Label(text="Add New Place:")
         self.name_label = Label(text="Name: ")
-        self.text_input = TextInput(multiline=False)
+        self.text_input = TextInput(write_tab=False, multiline=False)
         self.country_label = Label(text="Country: ")
-        self.country_input = TextInput(multiline=False)
+        self.country_input = TextInput(write_tab=False, multiline=False)
         self.priority_label = Label(text="Priority: ")
-        self.priority_input = TextInput(multiline=False)
-        self.filler9 = Label(text="Bruh")
-        self.filler10 = Label(text="Bruh")
+        self.priority_input = TextInput(write_tab=False, multiline=False)
+        self.add_button = Button(text="Add song")
+        self.clear_button = Button(text="Clear Fields")
 
     def build(self):
         self.title = "TravelTracker 2.0 by Zwe Nyan Toe"
@@ -41,7 +43,7 @@ class TravelTracker(App):
 
     def build_widgets_left_col(self):
         self.root.ids.left_layout.add_widget(self.sort_label)
-        self.root.ids.left_layout.add_widget(self.filler1)
+        self.root.ids.left_layout.add_widget(self.spinner)
         self.root.ids.left_layout.add_widget(self.add_label)
         self.root.ids.left_layout.add_widget(self.name_label)
         self.root.ids.left_layout.add_widget(self.text_input)
@@ -49,7 +51,7 @@ class TravelTracker(App):
         self.root.ids.left_layout.add_widget(self.country_input)
         self.root.ids.left_layout.add_widget(self.priority_label)
         self.root.ids.left_layout.add_widget(self.priority_input)
-        self.root.ids.left_layout.add_widget(self.filler9)
-        self.root.ids.left_layout.add_widget(self.filler10)
+        self.root.ids.left_layout.add_widget(self.add_button)
+        self.root.ids.left_layout.add_widget(self.clear_button)
 
 TravelTracker().run()
